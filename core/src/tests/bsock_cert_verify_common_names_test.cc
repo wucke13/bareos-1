f8a5f28b998870c6c2257bce6e8f7fc893bcd37b
Philipp Storz <philipp.storz@bareos.com>
2021-03-16 17:30:54 +0100

    removed alist.h not needed in bsock_cert_verify_common_names_test

--- core/src/tests/bsock_cert_verify_common_names_test.cc
+++ core/src/tests/bsock_cert_verify_common_names_test.cc
@@ -1,7 +1,7 @@
 /*
    BAREOS® - Backup Archiving REcovery Open Sourced
 
-   Copyright (C) 2018-2019 Bareos GmbH & Co. KG
+   Copyright (C) 2018-2021 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -27,7 +27,6 @@
 #  include "include/bareos.h"
 #endif
 
-#include "lib/alist.h"
 #include "lib/tls_conf_cert.h"
 
 TEST(bsock, config_tls_cert_verify_common_names_list_test)
